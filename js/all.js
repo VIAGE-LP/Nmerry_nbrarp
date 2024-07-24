39dbc536a08dd0eacde53b60ed8fc687fc28753e
Pickachu Chen <pickachu1006@gmail.com>
2024-07-24 18:11:10 +0800

    20240724_event & timer

--- js/all.js
+++ js/all.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
     // 定义点击事件处理程序
-    function scrollToSize(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('#size').offset().top
-        }, 900);
-    }
-    $('a[href="#size"]').click(scrollToSize);
-    $('.coupon_btn').click(scrollToSize);
+    // function scrollToSize(e) {
+    //     e.preventDefault();
+    //     $('html, body').animate({
+    //         scrollTop: $('#size').offset().top
+    //     }, 900);
+    // }
+    // $('a[href="#size"]').click(scrollToSize);
+    // $('.coupon_btn').click(scrollToSize);
 
     // copy折扣碼函式
-    function copybtn(money, couponcode) {
-        // 複製（利用 Clipboard API 中的寫入功能）
-        navigator.clipboard.writeText(couponcode);
-        // 提醒視窗
-        confirm(`恭喜您成功複製折扣碼！\n期間內不限方案，\n請於購物車「優惠代碼」欄位輸入 即可享優惠金額再折${money}元\n優惠代碼:${couponcode}`);
-    }
+    // function copybtn(money, couponcode) {
+    //     // 複製（利用 Clipboard API 中的寫入功能）
+    //     navigator.clipboard.writeText(couponcode);
+    //     // 提醒視窗
+    //     confirm(`恭喜您成功複製折扣碼！\n期間內不限方案，\n請於購物車「優惠代碼」欄位輸入 即可享優惠金額再折${money}元\n優惠代碼:${couponcode}`);
+    // }
 
-    $(".copybtn").on("click", function () {
-        copybtn(80, "nbra24ss");
-    });
+    // $(".copybtn").on("click", function () {
+    //     copybtn(80, "nbra24ss");
+    // });
 
     // $(".coupon_200").on("click", function () {
     //     copybtn(200, "200N2406");
@@ -33,10 +33,10 @@ $(document).ready(function () {
 
 
     //時間到隱藏素材
-    const currentDate = new Date();
-    const event_end = new Date('2024/07/10 23:59:59');
-    if(currentDate >= event_end){
-        $(".event2_hide").hide();
-    }
+    // const currentDate = new Date();
+    // const event_end = new Date('2024/07/10 23:59:59');
+    // if(currentDate >= event_end){
+    //     $(".event2_hide").hide();
+    // }
 
 });
--- js/countdown.js
+++ js/countdown.js
@@ -1,33 +1,18 @@
-const countdown = ()=>{
+const countdown = () => {
   const now = new Date();
-  //倒數計時開始時間
-  const target_start = new Date('2024/07/22 00:00:00'); 
-  const differ_start = target_start.getTime() - now.getTime();
-  //倒數計時開始區間
-  const target_mid_start = new Date('2024/07/22 00:00:00'); 
-  const differ_mid_start = target_mid_start.getTime() - now.getTime();
-  //倒數計時結束區間
-  const target_mid_end = new Date('2024/07/25 00:00:00'); 
-  const differ_mid_end = target_mid_end.getTime() - now.getTime();
-  //倒數計時結束時間
-  const target_end = new Date('2024/07/25 23:59:59'); 
-  const differ_end = target_end.getTime() - now.getTime();
-// console.log(differ_end)
-//  console.log(differ_start)
-// console.log(differ_mid_start)
-// console.log(differ_mid_end)
+  const day_start = new Date('2024/07/22 00:00:00'); // 天倒數 開始時間
+  const sec_start = new Date('2024/07/25 00:00:00'); // 秒倒數 開始時間
+  const end = new Date('2024/07/25 23:59:59'); // 結束時間
 
-if((differ_end > 0) && (differ_start < 0) && ((differ_mid_start > 0) || (differ_mid_end < 0))){
-  $("#cdtimer").show();
-  $("#cdtimer_day").hide();
-}else if((differ_end > 0) && (differ_start < 0) && (differ_mid_start < 0) && (differ_mid_end > 0)){
-  $("#cdtimer").hide();
-  $("#cdtimer_day").show();
-}else if((differ_end < 0)){
-  $("#cdtimer").hide();
-  $("#cdtimer_day").hide();
-}
+  if ((now >= day_start) && (now < sec_start)) {
+    $("#cdtimer_day").show();
+    $("#cdtimer").hide();
+  } else if ((now >= sec_start) && (now < end)) {
+    $("#cdtimer_day").hide();
+    $("#cdtimer").show();
+  }
 
+  const differ_end = end.getTime() - now.getTime();
   const ms = Math.floor(differ_end );
   const sec = Math.floor(differ_end  / 1000 % 60);
   const min = Math.floor(differ_end / 1000 / 60 % 60);
@@ -38,10 +23,10 @@ if((differ_end > 0) && (differ_start < 0) && ((differ_mid_start > 0) || (differ_
   document.querySelector('#countdown .seconds').textContent = String(sec).padStart(2, '0');
   document.querySelector('#countdown .minutes').textContent = String(min).padStart(2, '0');
   document.querySelector('#countdown .hours').textContent = String(hour).padStart(2, '0');
-  document.querySelectorAll('#countdown .day').textContent = String(day).padStart(2, '0');
   for(i=0;i<=document.querySelectorAll('#countdown .day').length;i++){
     if(document.querySelectorAll('#countdown .day')[i]!=undefined)
     document.querySelectorAll('#countdown .day')[i].textContent = String(day).padStart(2, '0');
   }
+
 }
 setInterval(countdown, 1000);
