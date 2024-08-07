f829d2015416123358af6366594ac05ac84c5aaf
Pickachu Chen <pickachu1006@gmail.com>
2024-08-07 15:21:45 +0800

    20240807_copy code btn

--- js/all.js
+++ js/all.js
@@ -1,42 +1,21 @@
 $(document).ready(function () {
-    // 定义点击事件处理程序
-    // function scrollToSize(e) {
-    //     e.preventDefault();
-    //     $('html, body').animate({
-    //         scrollTop: $('#size').offset().top
-    //     }, 900);
-    // }
-    // $('a[href="#size"]').click(scrollToSize);
-    // $('.coupon_btn').click(scrollToSize);
+    $('a[href="#size"]').click(function (e) {
+        e.preventDefault();
+        $('html , body').animate({
+            scrollTop: $('#size').offset().top,
+        }, 900);
+    });
 
-    // copy折扣碼函式
-    // function copybtn(money, couponcode) {
-    //     // 複製（利用 Clipboard API 中的寫入功能）
-    //     navigator.clipboard.writeText(couponcode);
-    //     // 提醒視窗
-    //     confirm(`恭喜您成功複製折扣碼！\n期間內不限方案，\n請於購物車「優惠代碼」欄位輸入 即可享優惠金額再折${money}元\n優惠代碼:${couponcode}`);
-    // }
+    //copy折扣碼 
+    function copybtn(couponcode) {
+        // 複製（利用 Clipboard API 中的寫入功能）
+        navigator.clipboard.writeText(couponcode);
+        // 提醒視窗
+        confirm(`恭喜您搶到「限量折扣碼」!\n您已成功複製折扣碼，請於購物車「優惠代碼」欄位輸入\n即可享優惠金額再折$100元。\n優惠代碼 : ${couponcode}`)
+    }
 
-    // $(".copybtn").on("click", function () {
-    //     copybtn(80, "nbra24ss");
-    // });
-
-    // $(".coupon_200").on("click", function () {
-    //     copybtn(200, "200N2406");
-    // });
-    // $(".coupon_150").on("click", function () {
-    //     copybtn(150, "150N2406","");
-    // });
-    // $(".coupon_100").on("click", function () {
-    //     copybtn(100, "100N2406","");
-    // });
-
-
-    //時間到隱藏素材
-    // const currentDate = new Date();
-    // const event_end = new Date('2024/07/10 23:59:59');
-    // if(currentDate >= event_end){
-    //     $(".event2_hide").hide();
-    // }
+    $(".copybtn").on("click", function () {
+        copybtn("24ns08ss");
+    });
 
 });
